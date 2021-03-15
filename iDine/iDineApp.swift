1201e37e773821d619d4d9f83d3db640fac575ca
Volodymyr7711 <volodumurbodnarchuk70@gmail.com>
2021-03-15 13:44:11 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Volodymyr Mendyk on 15.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
